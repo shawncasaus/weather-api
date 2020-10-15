378de941d30315d21f7c6b3458a13e5c4b8fc6b7
Shawn Casaus <shawn.casaus@gmail.com>
2020-10-15 08:49:23 -0700

    Delete api-key.js

--- src/utils/api-key.js
+++
@@ -1,6 +0,0 @@
-const keys = {
-    darkSky: 'c02e37e919e27d324b0041491fb87d83',
-    mapBox: 'pk.eyJ1Ijoic2hhd25jYXNhdXMiLCJhIjoiY2s1dzIxcDA5MTh3MTNqb2c5bGhveTN3OSJ9.b0vMHRkFISA_78u4tSGwLg'
-}
-
-module.exports = keys;
